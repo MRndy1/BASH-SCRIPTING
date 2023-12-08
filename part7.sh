65825de6566c75c80b27e95135d2b8264a7b8bdb
MRndy1 <123948911+MRndy1@users.noreply.github.com>
2023-12-08 12:08:22 +0700

    change part7.sh

--- part7.sh
+++ part7.sh
@@ -3,12 +3,12 @@
 echo "how to write a function
 function function_name() {
     the code
-    }" | cowsay 
+    }"
 
 function test_shadow(){
     if [ -e /etc/shadow ];
     then
-        echo "YES" | lolcat -a -d 100
+        echo "YES"
     else
         echo "NO"
     fi
